4b7465a85bb56bb166b6f38c0e25c4c9a5eb7960
Ruban <rubans5400@gmail.com>
2024-06-06 16:32:51 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/example/medicall_exhibitor/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.medicall_exhibitor
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+import 'Screens/splash_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Medicall Exhibitor',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
+        useMaterial3: true,
+      ),
+      home: const SplashScreen(),
+    );
+  }
+}
