9cbe013152dceac80bc66de0f0ff7589cb8af2c4
Ruban <rubans5400@gmail.com>
2024-06-16 03:21:42 +0530

    event id corrected

--- lib/Exhibitor/Controllers/local_data.dart
+++ lib/Exhibitor/Controllers/local_data.dart
@@ -1,8 +1,20 @@
 import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
+import 'package:get_storage/get_storage.dart';
 
 class LocalDataProvider extends ChangeNotifier {
-  int eventId = 0;
+  late Map<String, dynamic> userDetails;
+  late int eventId;
+
+  LocalDataProvider() {
+    var storedData = GetStorage().read("local_store");
+    if (storedData != null && storedData != '') {
+      userDetails = storedData;
+      eventId = userDetails['current_event_id'] ?? 0;  // default value if key doesn't exist
+    } else {
+      userDetails = {};
+      eventId = 0;  // default value when no stored data is found
+    }
+  }
 
   void changeEventID(int id) {
     eventId = id;
@@ -10,10 +22,21 @@ class LocalDataProvider extends ChangeNotifier {
   }
 }
 
-// Future<int> fetchEventId() async {
-//   // Simulate a network request
-//   await Future.delayed(Duration(seconds: 2));
-//   eventId = 17; // Simulated fetched data
-//   notifyListeners();
-//   return eventId;
+
+// old code
+// import 'package:flutter/foundation.dart';
+// import 'package:flutter/material.dart';
+// import 'package:get_storage/get_storage.dart';
+//
+// class LocalDataProvider extends ChangeNotifier {
+//
+//   final UserDetails = GetStorage().read("local_store") != ''
+//       ? GetStorage().read("local_store")
+//       : '';
+//   int eventId = 0;
+//
+//   void changeEventID(int id) {
+//     eventId = id;
+//     notifyListeners();
+//   }
 // }
